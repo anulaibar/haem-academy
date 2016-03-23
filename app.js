d3a18155178f95d514545040484e2d6eabf19917
Olle Hellgren <olle.hellgren@blocket.se>
2016-03-23 12:25:40 +0100

    Fixed port setting on Heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(3000, function () {
-  console.log('Express started on port 3000...');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express started on port ' + port + '...');
 });
